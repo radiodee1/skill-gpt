9177c7dbe15196f95e7c7d684a4f79aae28689d3
David Liebman <radiodee1@gmail.com>
2022-06-12 14:22:38 -0400

    query empty model classes

--- src/query_gpt_model.py
+++ src/query_gpt_model.py
@@ -2,6 +2,7 @@
 
 import argparse
 import sys
+from os.path import exists
 
 if len(sys.argv) > 1:
     txtname = sys.argv[1]
@@ -20,6 +21,7 @@ class DefaultGPT:
 
     file_name = "default"
     input_line = "Hello there."
+    is_online = False
     url = "https://"
     header = {}
     body = {}
@@ -31,6 +33,7 @@ class DefaultGPT:
         self.url = "https://"
         self.header = {}
         self.body = {}
+        self.is_online = False
 
 
     def setup(self):
@@ -46,7 +49,7 @@ class DefaultGPT:
         if self.model == "default":
             return self.input_line
         else:
-            return ""
+            return self.model
 
     def set_model(self, model):
         self.model = model
@@ -61,51 +64,86 @@ class GPT2(DefaultGPT):
     def __init__(self):
         DefaultGPT.__init__(self)
         self.model = "gpt2"
-        self.file_name = "gpt2"
+        self.file_name = args.tabname.split('.')[0] + '.' + self.model + ".query.tsv"
         self.input_line = "Hello there."
         self.url = "https://"
         self.header = {}
         self.body = {}
+        self.is_online = False
 
 
 class GPTJ(DefaultGPT):
     def __init__(self):
         DefaultGPT.__init__(self)
         self.model = "gptj"
-        self.file_name = "gptj"
+        self.file_name = args.tabname.split('.')[0] + '.' + self.model + ".query.tsv"
         self.input_line = "Hello there."
         self.url = "https://"
         self.header = {}
         self.body = {}
+        self.is_online = False
 
 
 class GPT3(DefaultGPT):
     def __init__(self):
         DefaultGPT.__init__(self)
         self.model = "gpt3"
-        self.file_name = "gpt3"
+        self.file_name = args.tabname.split('.')[0] + '.' + self.model + ".query.tsv"
         self.input_line = "Hello there."
         self.url = "https://"
         self.header = {}
         self.body = {}
-
-
+        self.is_online = True
 
 
 if __name__ == "__main__":
     gpt = DefaultGPT()
 
     if args.model == "gpt2":
-        print("gpt2")
+        print("gpt2 model")
         gpt = GPT2()
     elif args.model == "gptj":
-        print("gptj")
+        print("gptj model")
         gpt = GPTJ()
     elif args.model == "gpt3":
-        print("gpt3")
+        print("gpt3 model")
         gpt = GPT3()
 
     gpt.setup()
 
     print(gpt.model)
     print(gpt.file_name)
+
+    if not exists("../data/" + gpt.file_name):
+        save = open("../data/" + gpt.file_name, "w")
+        file = open("../data/" + args.tabname, "r")
+        j = []
+        num = 0
+        for l in file:
+            print (l.strip())
+            j.append(l.split("\t")[0])
+            print(j[num])
+            save.write(j[num] + "\t" + "\t" +  "\n")
+            num += 1 
+            if num >= args.length: break
+        save.close()
+        file.close()
+
+    if exists("../data/" + gpt.file_name):
+        saved = open("../data/" + gpt.file_name, 'r')
+        x = []
+        for l in saved:
+            l = l.split("\t")
+            if len(l) > 1 and len(l[1]) > 1:
+                pass
+            elif (len(l) > 1 and len(l[1]) <= 1) or len(l) == 1:
+                if len(l) == 1: l.append("")
+                ## replace l[1]
+                gpt.input_line = l[0]
+                l[1] = gpt.get_response()
+                print("num" , l[1])
+            x.append([ l[0], l[1] ])
+        resave = open("../data/" + gpt.file_name, "w")
+        for l in x:
+            resave.write(l[0] + "\t" + l[1] + "\n")
+        resave.close()
