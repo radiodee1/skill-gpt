7b2d1c98c354d6f909cc4cc70862f6fcc96581f4
David Liebman <radiodee1@gmail.com>
2022-06-16 16:02:27 -0400

    print to file

--- src/total_count_overall.py
+++ src/total_count_overall.py
@@ -12,7 +12,7 @@ if len(sys.argv) > 1:
 parser = argparse.ArgumentParser(description='Make tab file from the movie corpus file using gpt engines.')
 parser.add_argument('--model_list', metavar='MODEL', default="gpt2,gpt2-medium,gpt2-large,gpt2-xl,gpt3" ,type=str, help='Code word GPT model list. Series of several strings ("gpt2", "gptj", "gpt3").')
 parser.add_argument("--tabname", default="tabname.tsv", type=str, help="Output tab file name.")
-parser.add_argument('--length', default=1000, type=int, help="Length, in sentences, of output file.")
+#parser.add_argument('--length', default=1000, type=int, help="Length, in sentences, of output file.")
 parser.add_argument("--screen", action="store_true", help="Print verbose output to screen.")
 parser.add_argument("--num_repeats", default=-1, type=int, help="Number of repeats to report on-screen.")
 args = parser.parse_args()
@@ -20,6 +20,7 @@ args = parser.parse_args()
 if __name__ == "__main__":
     repeats = 0
     z = 0 
+    file_name_out = args.tabname.split('.')[0] + "."
     if args.num_repeats > -1: repeats = args.num_repeats
     engine_record = []
     engine_visual = []
@@ -29,6 +30,7 @@ if __name__ == "__main__":
         ii = models[i]
         file_name_in =  args.tabname.split('.')[0] + '.' + ii + ".compare.tsv"
         if exists("../data/" + file_name_in):
+            file_name_out += ii + "_"
             file = open("../data/" + file_name_in, "r")
             if args.num_repeats == -1: repeats += 1 
             for j in file:
@@ -47,10 +49,16 @@ if __name__ == "__main__":
                 z += 1 
                 
         pass
-    print("---")
+    if args.screen: print("---")
     for k in range(len(engine_record)):
         if engine_record[k][3] >= repeats:
             engine_visual.append([ engine_record[k][3], engine_record[k][1], engine_record[k][2] ])
             print(engine_record[k][3], engine_record[k][1], engine_record[k][2])
     print(len(engine_visual))
 
+    file_name_out += str(len(engine_visual)) + ".totals.tsv"
+    print(file_name_out)
+    save = open("../data/"+ file_name_out, "w")
+    for n in engine_visual:
+        save.write(str(n[0]) + "\t" + str(n[1]) + "\t" + str(n[2]) + "\n")
+    save.close()
